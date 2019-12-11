db20c0397a4f0911822a935a3524550dfc99a89b
Julien Jakubowski <julien@jakubowski.fr>
2019-12-11 21:27:58 +0100

    pass when multiple of 3

--- foobarqix.js
+++ foobarqix.js
@@ -1,4 +1,5 @@
 function compute(number) {
+    if(number % 3 === 0) return 'Foo';
     return number
 }
 module.exports = compute;
--- foobarqix.test.js
+++ foobarqix.test.js
@@ -4,3 +4,9 @@ test('returns the same number when no particular rule applies', () => {
   expect(compute('1')).toBe('1');
   expect(compute('2')).toBe('2');
 });
+
+test('returns Foo when multiple of 3', () => {
+  expect(compute('6')).toBe('Foo');
+  expect(compute('99')).toBe('Foo');
+});
+
