284d3fc264f1fb054a72b9164953a9e9e6c2bcf1
Julien Jakubowski <julien@jakubowski.fr>
2019-12-11 21:23:49 +0100

    pass when no particular rules applies

--- foobarqix.js
+++ foobarqix.js
@@ -1,4 +1,4 @@
 function compute(number) {
-    return '1'
+    return number
 }
 module.exports = compute;
--- foobarqix.test.js
+++ foobarqix.test.js
@@ -2,4 +2,5 @@ const compute = require('./foobarqix');
 
 test('returns the same number when no particular rule applies', () => {
   expect(compute('1')).toBe('1');
+  expect(compute('2')).toBe('2');
 });
