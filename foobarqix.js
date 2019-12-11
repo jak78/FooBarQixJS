b17204978d1c47ec8a852e15b466cf66ad96ff9f
Julien Jakubowski <julien@jakubowski.fr>
2019-12-11 21:38:56 +0100

    pass when multiple of 3, 5 or 7

--- foobarqix.js
+++ foobarqix.js
@@ -2,6 +2,7 @@ function compute(number) {
     let results = [];
     if (number % 3 === 0) results.push('Foo');
     if (number % 5 === 0) results.push('Bar');
+    if (number % 7 === 0) results.push('Qix');
     if (results.length === 0) {
         return number;
     } else {
--- foobarqix.test.js
+++ foobarqix.test.js
@@ -12,7 +12,16 @@ test('returns Bar when multiple of 5', () => {
   expect(compute('10')).toBe('Bar');
 });
 
+test('returns Qix when multiple of 7', () => {
+  expect(compute('14')).toBe('Qix');
+});
+
 test('returns FooBar when multiple of both 3 and 5', () => {
   expect(compute('30')).toBe('FooBar');
 });
+
+test('returns FooBarQix when multiple of 3, 5 & 7', () => {
+  expect(compute('210')).toBe('FooBarQix');
+});
+
 
